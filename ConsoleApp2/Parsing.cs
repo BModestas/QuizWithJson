697e37b5efc078db07bc87bfbc1b5cf5dc96bb37
BModestas <115711308+BModestas@users.noreply.github.com>
2023-01-10 14:22:44 +0200

    new file:   .vs/ConsoleApp2/FileContentIndex/1c270b7c-1de0-429e-9a7c-8a8282fc374b.vsidx 	new file:   .vs/ConsoleApp2/FileContentIndex/c203abde-a44b-46dd-8a86-9367191487ad.vsidx 	new file:   .vs/ConsoleApp2/FileContentIndex/d41148f3-5b11-474e-a63b-7b7cd82a58e5.vsidx 	new file:   .vs/ConsoleApp2/FileContentIndex/read.lock 	new file:   .vs/ConsoleApp2/v17/.suo 	new file:   .vs/ConsoleApp2/v17/TestStore/0/000.testlog 	new file:   .vs/ConsoleApp2/v17/TestStore/0/testlog.manifest 	new file:   ConsoleApp2.sln 	new file:   ConsoleApp2/App.config 	new file:   ConsoleApp2/ConsoleApp2.csproj 	new file:   ConsoleApp2/Parsing.cs 	new file:   ConsoleApp2/Properties/AssemblyInfo.cs 	new file:   ConsoleApp2/bin/Debug/ConsoleApp2.exe 	new file:   ConsoleApp2/bin/Debug/ConsoleApp2.exe.config 	new file:   ConsoleApp2/bin/Debug/ConsoleApp2.pdb 	new file:   ConsoleApp2/bin/Debug/Newtonsoft.Json.dll 	new file:   ConsoleApp2/bin/Debug/Newtonsoft.Json.xml 	new file:   ConsoleApp2/bin/Debug/test.json 	new file:   ConsoleApp2/obj/Debug/.NETFramework,Version=v4.7.2.AssemblyAttributes.cs 	new file:   ConsoleApp2/obj/Debug/ConsoleApp2.csproj.AssemblyReference.cache 	new file:   ConsoleApp2/obj/Debug/ConsoleApp2.csproj.CopyComplete 	new file:   ConsoleApp2/obj/Debug/ConsoleApp2.csproj.CoreCompileInputs.cache 	new file:   ConsoleApp2/obj/Debug/ConsoleApp2.csproj.FileListAbsolute.txt 	new file:   ConsoleApp2/obj/Debug/ConsoleApp2.csproj.SuggestedBindingRedirects.cache 	new file:   ConsoleApp2/obj/Debug/ConsoleApp2.exe 	new file:   ConsoleApp2/obj/Debug/ConsoleApp2.pdb 	new file:   ConsoleApp2/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   ConsoleApp2/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   ConsoleApp2/packages.config 	new file:   packages/Newtonsoft.Json.13.0.2/.signature.p7s 	new file:   packages/Newtonsoft.Json.13.0.2/LICENSE.md 	new file:   packages/Newtonsoft.Json.13.0.2/Newtonsoft.Json.13.0.2.nupkg 	new file:   packages/Newtonsoft.Json.13.0.2/README.md 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net20/Newtonsoft.Json.dll 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net20/Newtonsoft.Json.xml 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net35/Newtonsoft.Json.dll 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net35/Newtonsoft.Json.xml 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net40/Newtonsoft.Json.dll 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net40/Newtonsoft.Json.xml 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net45/Newtonsoft.Json.dll 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net45/Newtonsoft.Json.xml 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net6.0/Newtonsoft.Json.dll 	new file:   packages/Newtonsoft.Json.13.0.2/lib/net6.0/Newtonsoft.Json.xml 	new file:   packages/Newtonsoft.Json.13.0.2/lib/netstandard1.0/Newtonsoft.Json.dll 	new file:   packages/Newtonsoft.Json.13.0.2/lib/netstandard1.0/Newtonsoft.Json.xml 	new file:   packages/Newtonsoft.Json.13.0.2/lib/netstandard1.3/Newtonsoft.Json.dll 	new file:   packages/Newtonsoft.Json.13.0.2/lib/netstandard1.3/Newtonsoft.Json.xml 	new file:   packages/Newtonsoft.Json.13.0.2/lib/netstandard2.0/Newtonsoft.Json.dll 	new file:   packages/Newtonsoft.Json.13.0.2/lib/netstandard2.0/Newtonsoft.Json.xml 	new file:   packages/Newtonsoft.Json.13.0.2/packageIcon.png

---
+++ ConsoleApp2/Parsing.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp2
+{
+    public class Fruit
+    {
+        public int id { get; set; }
+        public string fruit { get; set; }
+        public string answerA { get; set; }
+        public string answerB { get; set; }
+        public int correct_answer { get; set; }
+        //use list if there is list item in json []
+        // public List<string> answers { get; set; }
+    }
+    public class Parsing
+    {
+        public static void Main(string[] args)
+        {
+            //Json file must be in bin>Debug directory
+            string json = System.IO.File.ReadAllText("test.json");
+            //install Newtonsoft nuGet
+            var people = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Fruit>>(json);
+            // taking every item in list
+            List<int> ids =new List<int>();
+            List<string> fruites = new List<string>();
+            List<string> answerAs = new List<string>();
+            List<string> answerBs = new List<string>();
+            List<int> correct_answers = new List<int>();
+            foreach (var item in people)
+            {
+                ids.Add(item.id);
+                fruites.Add(item.fruit); 
+                answerAs.Add(item.answerA);
+                answerBs.Add(item.answerB);
+                correct_answers.Add(item.correct_answer);
+            }
+            double count = 0;
+            for (int i = 0; i < ids.Count; i++)
+            {
+                Console.WriteLine("What color is " + fruites[i] + "?");
+                Console.WriteLine("1) " + answerAs[i]);
+                Console.Write("2) " + answerBs[i] + " ");
+                int ans = int.Parse(Console.ReadLine());               
+                if (ans == correct_answers[i])
+                {                                     
+                    count++;
+                }  
+            }
+            double percentage = (count * 100) / 3;
+            if (percentage > 65)
+            {
+                Console.WriteLine("Great your result is " + percentage + "% you pass!!!!");
+            }
+        }
+    }
+}
